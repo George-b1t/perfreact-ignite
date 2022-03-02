5640ee1768071482ce1132df5452657f578f4fd1
George-b1t <george.soares509@gmail.com>
2022-03-02 10:38:58 -0300

    utilizando react-virtualized para grandes listas

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { List, ListRowRenderer } from "react-virtualized";
 import { Product, ProductItem } from "./ProductItem";
 
 type SearchResultsProps = {
@@ -12,19 +12,26 @@ export function SearchResults({
   totalPrice,
   onAddWishlist,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem product={results[index]} onAddWishlist={onAddWishlist} />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map((product) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddWishlist={onAddWishlist}
-          />
-        );
-      })}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
